0882a2524a350208bb13fa5c30b8a1cba7787a47
ams-10 <abdullahshareefh@gmail.com>
2023-07-31 14:00:17 -0300

    no new msg

---
+++ typeCaste.java
@@ -0,0 +1,12 @@
+public class typeCaste{
+	public static void main(String args[]){
+		int x = 8;
+		double y = x;
+		System.out.println(x);
+		System.out.println(y);
+		double z=5.665;
+		int t = (int)z;
+		System.out.println(z);
+		System.out.println(t);
+	}
+}
