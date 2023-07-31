0ce36d5decd6a9766d1cb7758abdd76e3ba387e6
ams-10 <abdullahshareefh@gmail.com>
2023-07-31 14:23:36 -0300

    factorial program

---
+++ factorial.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+public class factorial{
+	public static void main(String args[]){
+		int i;
+		int fact = 1;
+		scanner sc = new scanner(System.in);
+		int num = sc.nextInt();
+		for(i=1;i<=num;i++){
+			fact=fact*i;
+		}
+		System.out.println("the factorial is : " + fact);
+	}
+}
