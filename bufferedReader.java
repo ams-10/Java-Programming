a1c359c46a5a389db4872199370dcce9de06d870
ams-10 <abdullahshareefh@gmail.com>
2023-07-31 16:53:01 -0300

    buffered reader

--- bufferedReader.java
+++ bufferedReader.java
@@ -1,6 +1,8 @@
 import java.io.*;
 public class bufferedReader{
 	public static void main(String args[]){
+		String var1,var2;
+			System.out.println("Enter any 5 numbers ");
 		InputStreamReader stdin = new InputStreamReader(System.in);
 		BufferedReader console = new BufferedReader(stdin);
 		int x[] = new int [5];
@@ -8,20 +10,20 @@ public class bufferedReader{
 		int z[] =  new int[5];
 		for(int i = 0 ; i < 5 ;i++){
 			try{
-				var1 = console.readline();
-				x[i] = Integer.parseInt(str1);
+				var1 = console.readLine();
+				x[i] = Integer.parseInt(var1);
 			}
 			catch(IOException e){}
-		System.out.println("Enter any 5 numbers ");
+	
 		try{
-			for(int i=0 ; i<5 ; i++){
-				var2 = console.readline();
-				y[i] = console.Integer.parseInt();
+			for(i=0 ; i<5 ; i++){
+				var2 = console.readLine();
+				y[i] = Integer.parseInt(var2);
 				z[i] = x[i] + y[i];
 			}
 		}catch(IOException e){}
 		System.out.println("the resultant array");
-		for(int i=0;i<5;i++){
+		for(i=0;i<5;i++){
 			System.out.println(z[i]);
 		}
 		}
