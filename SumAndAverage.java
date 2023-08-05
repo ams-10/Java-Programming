a8948ccb6abda78e681d944c40934a0f8ab361b1
ams-10 <abdullahshareefh@gmail.com>
2023-08-05 07:32:53 -0300

    Sum and average of given numbers

---
+++ SumAndAverage.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class SumAndAverage{
+	public static void main(String args[]){
+		System.out.println("Enter the number of inputs : ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int sum = 0;
+		
+		for(int i=0 ; i<n ; i++){
+			System.out.println("Enter the number : "+(i+1)+" : ");
+			int number = sc.nextInt();
+			sum += number;
+		}
+		System.out.println("Sum of the number : "+sum);
+		double avg = sum/n;
+		System.out.println("average of the numbers : "+avg);
+	}
+}
