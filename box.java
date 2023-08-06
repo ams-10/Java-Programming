0fa2f2dfdf29ec9d378705a08908f398c8f4016a
ams-10 <abdullahshareefh@gmail.com>
2023-08-06 08:53:34 -0300

    packages demonstration

---
+++ box.java
@@ -0,0 +1,8 @@
+package box;
+public class area{
+	int x = 10;
+	int y = 10;
+	void display(){
+		System.out.println("area is : "+(x+y));
+	}
+}
--- packageExample.java
+++ packageExample.java
@@ -1,9 +1,8 @@
-import SumAndAverage.*;
+import box.*;
 //declare first statement as >>package package_name; to create package
 class packageExample{
 	public static void main(String args[]){
-		System.out.println("running through other java file");
-		SumAndAverage objSA = new SumAndAverage();	
-		objSA.main();
+		box obj = new box();
+		obj.diplay();
 	}
 }
