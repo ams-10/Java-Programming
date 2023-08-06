f4c6f0deb74e960d0ea2305383b5a5404763ea09
ams-10 <abdullahshareefh@gmail.com>
2023-08-06 09:08:53 -0300

    packages demonstration

--- box.java
+++ box.java
@@ -1,8 +1,12 @@
 package box;
 public class area{
-	int x = 10;
-	int y = 10;
-	void display(){
+	public void display(){
+		int x = 10;
+		int y = 10;
 		System.out.println("area is : "+(x+y));
+	}
+	public static void main(String args[]){
+		area obj = new obj();
+		obj.display();
 	}
 }
--- packageExample.java
+++ packageExample.java
@@ -2,7 +2,7 @@ import box.*;
 //declare first statement as >>package package_name; to create package
 class packageExample{
 	public static void main(String args[]){
-		box obj = new box();
-		obj.diplay();
+		area obj = new area();
+		obj.display();
 	}
 }
