20d8ad0d4419720dd7823ac4b7be3887a9262cec
ams-10 <abdullahshareef123h@gmail.com>
2023-08-18 14:24:43 -0200

    List example

---
+++ ListDemo.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.ArrayList;
+public class ListDemo{
+	public static void main(String args[]){
+		List<String> city = new ArrayList<String>();
+		city.add("hyd");
+		city.add("mumbai");
+		city.add("delhi");
+		System.out.println(city);
+	}
+}
